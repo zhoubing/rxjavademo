3d11d13836c2158d69d9fc621d59cc7695c55df4
zhoubing <zhoubing19820720@hotmail.com>
2019-09-12 11:10:13 +0800

    有条件的retry

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,8 +3,9 @@ import io.reactivex.rxjava3.core.Observable
 fun main(args: Array<String>) {
     Observable.create<String> {
         println("working")
-        it.onError(Exception(""))
-    }.retry(1).subscribe({
+        it.onError(Exception("101002"))
+    }.retry(2) { t: Throwable? ->  t?.message == "101002"
+    }.subscribe({
         println("success")
     }, {
         println("error")
